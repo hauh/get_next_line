107947d40fe1229a5fe14024a793b579c2fec1f4
smorty <hauh@ya.ru>
2019-04-16 22:14:47 +0300

    gnl1

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: smorty <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 15:36:49 by smorty            #+#    #+#             */
-/*   Updated: 2019/04/15 23:24:27 by smorty           ###   ########.fr       */
+/*   Updated: 2019/04/16 22:13:35 by smorty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,61 @@
 
 #include <stdio.h>
 
-int get_next_line(const int fd, char **line)
+static int	find_newline(char *buf)
 {
-	char buf[BUFF_SIZE + 1];
-	int n;
-	size_t i;
-	static char *str;
+	char *buf0;
 
-	if (!str)
+	buf0 = buf;
+	while (*buf)
 	{
-		buf[BUFF_SIZE] = '\0';
-		str = ft_strdup(buf);
+		if (*buf == '\n')
+			return (buf - buf0);
+		buf++;
 	}
-	n = BUFF_SIZE;
-	while (!ft_strchr(str, 10)) && n >= BUFF_SIZE)
+	return (0);
+}
+
+int	gnl_lst(const int fd, char **line, t_str **list)
+{
+	char	buf[BUFF_SIZE + 1];
+	int n;
+	int i;
+
+	if (!*list)
+	{
+		*list = (t_str *)malloc(sizeof(t_str));
+		(*list)->s = (char *)malloc(sizeof(char));
+		*(*list)->s = '\0';
+		(*list)->file = fd;
+		(*list)->next = NULL;
+	}
+	if (fd != (*list)->file)
+		return (gnl_lst(fd, line, &(*list)->next));
+	while (!find_newline((*list)->s))
 	{
 		n = read(fd, buf, BUFF_SIZE);
-		buf[BUFF_SIZE] = '\0';
-		str = ft_strjoin(str, buf);
+		if (!n)
+			break ;
+		if (n < 0)
+			return (-1);
+		buf[n] = '\0';
+		(*list)->s = ft_strjoin((*list)->s, buf);
 	}
-	i = 0;
-	while (str[i] != '\n')
-		i++;
-	*line = ft_strndup(str, i);
-	str = ft_strchr(str, 10) + 1;
+	if (!*((*list)->s))
+		return (0);
+	i = find_newline((*list)->s);
+	if (!i)
+		return (0);
+	*line = ft_strndup((*list)->s, i);
+	(*list)->s = (*list)->s + i;
 	return (1);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static t_str *list = NULL;
+
+	return (gnl_lst(fd, line, &list));
 }
 
 int main(int argc, char **argv)
@@ -47,15 +77,15 @@ int main(int argc, char **argv)
 	unsigned int i;
 	int fd;
 
-	i = 0;
+	i = 1;
 	if (argc > 1)
 	{
 		fd = open(argv[1], O_RDONLY);
-		while (i < 10)
+		while (i)
 		{
-			printf("%d\n", get_next_line(fd, &line));
-			printf("%s\n", line);
-			i++;
+			printf("main:%d\n", i = get_next_line(fd, &line));
+			if (i)
+				printf("main:%s\n", line);
 		}
 	}
 	return (0);
--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,28 @@
 /*   By: smorty <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 15:53:26 by smorty            #+#    #+#             */
-/*   Updated: 2019/04/15 22:55:56 by smorty           ###   ########.fr       */
+/*   Updated: 2019/04/16 21:44:51 by smorty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include "libft/libft.h"
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include "libft/libft.h"
 
-#define BUFF_SIZE 10
+# define BUFF_SIZE 2
+
+typedef struct	s_str
+{
+	char	*s;
+	int		file;
+	struct s_str	*next;
+}				t_str;
+
+int				get_next_line(const int fd, char **line);
+int				gnl_lst(const int fd, char **line, t_str **list);
 
 #endif
